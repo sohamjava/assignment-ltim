8934e359766dc169b1f212952c77ae05e399bb5b
soham_sengupta_java <Soham Sengupta@host.docker.internal>
2025-01-31 22:33:45 +0530

    programmatically add new span

--- spring-observability-micrometer-prom/src/main/java/demoapp/service/SomeService.java
+++ spring-observability-micrometer-prom/src/main/java/demoapp/service/SomeService.java
@@ -4,9 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import brave.Span;
+import brave.Tracer;
 import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.tracing.Tracer.SpanInScope;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,18 +19,26 @@ import lombok.extern.slf4j.Slf4j;
 public class SomeService {
 	private final MeterRegistry  meterRegistry;
 	private final RestTemplate restTemplate;
+	private final Tracer tracer;
 	@Timed(value = "timed.doSomething", description = "doSomething")
 	@Counted(value = "counter.doSomething")
 
 	public String doSomething(int x) {
-		log.info("Received :: {}",x);
+		Span span = tracer.nextSpan().name("doSomething").start();
+		  try (brave.Tracer.SpanInScope ws = tracer.withSpanInScope(span.start())) {
+				log.info("Received :: {}",x);
 		
 		ResponseEntity<String> y=restTemplate.getForEntity("http://localhost:8787/app/a?p1=s", String.class);
 		if(x %3 ==0) {
 			throw new RuntimeException("Simulated exception -/ by 3");
 		}
+		
 		meterRegistry.counter("success.count").increment();
 		return y.getBody()+":: Thanks for enrolling "+x;
+		}finally {
+			span.finish();
+			log.info("End span");
+		}
 	}
 	
 }
